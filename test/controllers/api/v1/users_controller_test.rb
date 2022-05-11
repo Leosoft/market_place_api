d299781cf05db69438a4cf836010ee25e27cfd4e
Leosoft <leosoft3031@gmail.com>
2022-05-11 09:31:14 -0300

    Adds the user create endpoint

--- test/controllers/api/v1/users_controller_test.rb
+++ test/controllers/api/v1/users_controller_test.rb
@@ -1,15 +1,27 @@
 require "test_helper"
 
 class Api::V1::UsersControllerTest < ActionDispatch::IntegrationTest
-  setup do
-    @user = users(:one)
+
+  # En este primer Test se revisa la creacion de usuario enviando una peticion POST valida
+  # Entonces se revisa se revisa que un usuario adicional se encuentra en la base de datos
+  # El codigo de respuesta debe ser created (codigo de estado 201)
+  test "Should create user" do
+    assert_difference('User.count') do
+      post api_v1_user_url, params: {user: {email:
+                                              'test@test.org',password: '123456'}}, as: :json
+    end
+    assert_response :created
   end
 
-  test 'Should show user' do
-    get api_v1_user_url(@user), as: :json
-    assert_response :success
 
-    json_response = JSON.parse(self.response.body)
-    assert_equal @user.email, json_response['email']
+  # En este segundo test se revisa que el usuario no sea creado usando un email que ya se encuentra en uso
+  # Entonces se revisa que el codigo http de respuesta sea unprocessable_entity (cod 422)
+
+  test "Should not create user with taken email" do
+    assert_no_difference('User.count') do
+      post api_v1_user_url, params: {user: {email:
+                                            @user.email, password: '123456'}}, as: :json
+    end
+    assert_response :unprocessable_entity
   end
 end
